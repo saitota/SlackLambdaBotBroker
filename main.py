4954deaaeedab00fc6b77547673752e1c054da4a
saitota <1152469+saitota@users.noreply.github.com>
2018-03-20 11:18:07 +0900

    refactor

--- main.py
+++ main.py
@@ -7,70 +7,74 @@ print('Loading function... ')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
+def event_to_dict(event):
+    if 'body' in event:
+        body = json.loads(event.get('body'))
+        return body
+    elif 'token' in event:
+        body = event
+        return body
+    else:
+        logger.error('unexpected event format')
+        exit
+
+
+class ChallangeJson(object):
+    def data(self, key):
+        return {
+            'isBase64Encoded': 'true',
+            'statusCode': 200,
+            'headers': {},
+            'body': key
+        }
+
+
+def invoke_function(function_name, body):
+    logger.info('call: %s', function_name)
+    client = boto3.client("lambda")
+    res = client.invoke(
+        FunctionName=function_name,
+        InvocationType="Event",
+        Payload=json.dumps(body))
+    return
+
+
 def handler(event, context):
     #getenv
     HOOK_CHANNEL = os.environ['HOOK_CHANNEL']
     BOT_NAME = os.environ['BOT_NAME']
-    #HOOK_REACTION = os.environ['HOOK_REACTION']
     HOOK_REACTIONS = os.environ['HOOK_REACTIONS']
     HOOK_KEYWORD = os.environ['HOOK_KEYWORD']
     FNC_REACT = os.environ['FNC_REACT']
     FNC_REPLY = os.environ['FNC_REPLY']
     FNC_ANONY = os.environ['FNC_ANONY']
-    
-    #受信したjsonをLogsに出力
+
+    # Output the received event to the log
     logging.info(json.dumps(event))
-    
-    #print (type(event))
-    # json処理
-    if 'body' in event:
-        body = json.loads(event.get('body'))
-    elif 'token' in event:
-        body = event
-    else:
-        logger.error('unexpected event format')
-        return {'statusCode': 500, 'body': 'error:unexpected event format'}
-        
-    #url_verificationイベントに返す
+    body = event_to_dict(event)
+
+    # return if it was challange-event
     if 'challenge' in body:
-        challenge = body.get('challenge')
-        logging.info('return challenge key %s:', challenge)
-        return {
-            'isBase64Encoded': 'true',
-            'statusCode': 200,
-            'headers': {},
-            'body': challenge
-        }
-        
-    client = boto3.client("lambda")
+        challenge_key = body.get('challenge')
+        logging.info('return challenge key %s:', challenge_key)
+        return ChallangeJson().data(challenge_key)
+
     # SlackAnonymousChannel
-    if body.get('event').get('channel','') == HOOK_CHANNEL and body.get('event').get('username') != BOT_NAME:
-        logger.info('hit: %s',HOOK_CHANNEL)
-        logger.info('call: %s',FNC_ANONY)
-        res = client.invoke(
-            FunctionName=FNC_ANONY,
-            InvocationType="Event",
-            Payload=json.dumps(body)
-        )
+    if body.get('event').get('channel', '') == HOOK_CHANNEL and body.get(
+            'event').get('username') != BOT_NAME:
+        invoke_function(FNC_ANONY, body)
+
     # SlackReplyBot
-    if HOOK_KEYWORD in body.get('event').get('text',''):
-        logger.info('hit: %s',HOOK_KEYWORD)
-        logger.info('call: %s',FNC_REPLY)
-        res = client.invoke(
-            FunctionName=FNC_REPLY,
-            InvocationType="Event",
-            Payload=json.dumps(body)
-        )
+    if HOOK_KEYWORD in body.get('event').get('text', ''):
+        logger.info('hit: %s', HOOK_KEYWORD)
+        invoke_function(FNC_REPLY, body)
+
     # SlackServerlessReactionBot
     emojis = HOOK_REACTIONS.split(',')
     if body.get('event').get('reaction') in emojis:
-        logger.info('hit: %s',body.get('event').get('reaction'))
-        logger.info('call: %s',FNC_REACT)
-        res = client.invoke(
-            FunctionName=FNC_REACT,
-            InvocationType="Event",
-            Payload=json.dumps(body)
-        )
-        
+        logger.info('hit: %s', body.get('event').get('reaction'))
+        invoke_function(FNC_REACT, body)
+
     # exit always normally
     return {'statusCode': 200, 'body': 'quit'}
